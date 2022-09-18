53f222ea9c87d756d130b7c4133b99b3d7c304e2
Biplab Dutta <biplabdutta860@gmail.com>
2022-09-18 16:01:54 +0545

    chore: add bootstrap method

---
+++ lib/bootstrap.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:now_in_dart_flutter/app/app_bloc_observer.dart';
+import 'package:now_in_dart_flutter/features/core/data/data.dart';
+
+typedef _BootstrapBuilder = Widget Function(Dio dio);
+
+void bootstrap(_BootstrapBuilder builder) {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  Bloc.observer = AppBlocObserver();
+
+  runZonedGuarded(
+    () async {
+      await IsarDatabase().init();
+      final dio = Dio()
+        ..options = BaseOptions(
+          baseUrl: 'api.github.com',
+          headers: {
+            'Accept': 'application/vnd.github.html+json',
+          },
+          responseType: ResponseType.plain,
+          validateStatus: (status) =>
+              status != null && status >= 200 && status < 400,
+        );
+      builder(dio);
+    },
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+  );
+}
