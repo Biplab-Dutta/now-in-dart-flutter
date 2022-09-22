d5dda30613669bcaf20effce8b660911ddc21f2f
Biplab Dutta <biplabdutta860@gmail.com>
2022-09-22 17:53:11 +0545

    create a singleton instance for the database class

--- lib/features/core/data/isar_database.dart
+++ lib/features/core/data/isar_database.dart
@@ -3,6 +3,11 @@ import 'package:now_in_dart_flutter/features/core/data/github_header.dart';
 import 'package:now_in_dart_flutter/features/detail/core/data/detail_dto.dart';
 
 class IsarDatabase {
+  factory IsarDatabase() => _isarDatabase;
+  IsarDatabase._internal();
+
+  static final _isarDatabase = IsarDatabase._internal();
+
   late Isar _instance;
 
   Isar get instance => _instance;
@@ -14,7 +19,6 @@ class IsarDatabase {
     if (Isar.instanceNames.isNotEmpty) return;
     _instance = await Isar.open(
       [GithubHeaderSchema, DetailDTOSchema],
-      inspector: false,
     );
   }
 }
