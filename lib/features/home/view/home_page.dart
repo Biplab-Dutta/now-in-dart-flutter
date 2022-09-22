350f0bbba7227a0f3a866e5fe9bddfd20ef74c03
Biplab Dutta <biplabdutta860@gmail.com>
2022-09-22 17:40:47 +0545

    feat: add hope page

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:now_in_dart_flutter/features/detail/dart_detail/presentation/view/dart_detail_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:now_in_dart_flutter/features/core/presentattion/assets_path.dart';
+import 'package:now_in_dart_flutter/features/core/presentattion/lazy_indexed_stack.dart';
+import 'package:now_in_dart_flutter/features/detail/dart_detail/presentation/view/dart_changelog_page.dart';
 import 'package:now_in_dart_flutter/features/detail/flutter_detail/presentation/view/flutter_detail_page.dart';
 import 'package:now_in_dart_flutter/features/home/cubit/home_cubit.dart';
 
@@ -18,28 +21,43 @@ class HomePage extends StatelessWidget {
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    const pages = <Widget>[DartDetailPage(), FlutterDetailPage()];
+    const pages = <Widget>[DartChangelogPage(), FlutterDetailPage()];
+
     final selectedTabIndex = context.select<HomeCubit, int>(
       (HomeCubit cubit) => cubit.state.index,
     );
     return Scaffold(
-      body: pages.elementAt(selectedTabIndex),
+      body: LazyIndexedStack(
+        index: selectedTabIndex,
+        children: pages,
+      ),
       bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Dart',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.notifications),
-            label: 'Flutter',
-          ),
-        ],
+        destinations: _destinations,
         selectedIndex: selectedTabIndex,
         onDestinationSelected: context.read<HomeCubit>().setTab,
       ),
     );
   }
+
+  static final _destinations = <NavigationDestination>[
+    NavigationDestination(
+      icon: SvgPicture.asset(
+        AssetsPath.dartIcon,
+        width: 24,
+        height: 24,
+      ),
+      label: 'Dart',
+    ),
+    NavigationDestination(
+      icon: SvgPicture.asset(
+        AssetsPath.flutterIcon,
+        width: 24,
+        height: 24,
+      ),
+      label: 'Flutter',
+    ),
+  ];
 }
