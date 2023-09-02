2ba055b03f679a18d07d9aa4299dfb89530af051
Biplab Dutta <biplabdutta860@gmail.com>
2023-09-03 00:40:16 +0545

    migrate `flutter_detail_bloc.dart` to fpdart

--- lib/features/detail/flutter_detail/application/flutter_detail_bloc.dart
+++ lib/features/detail/flutter_detail/application/flutter_detail_bloc.dart
@@ -1,17 +1,17 @@
-import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:now_in_dart_flutter/core/domain/failure.dart';
 import 'package:now_in_dart_flutter/core/domain/fresh.dart';
 import 'package:now_in_dart_flutter/features/detail/core/domain/detail.dart';
-import 'package:now_in_dart_flutter/features/detail/core/domain/detail_failure.dart';
 import 'package:now_in_dart_flutter/features/detail/flutter_detail/data/flutter_detail_repository.dart';
 
 part 'flutter_detail_bloc.freezed.dart';
 part 'flutter_detail_event.dart';
 part 'flutter_detail_state.dart';
 
-typedef _DetailFailureOrSuccess = Future<Either<DetailFailure, Fresh<Detail>>>;
+typedef _DetailFailureOrSuccess = TaskEither<Failure, Fresh<Detail>>;
 
 class FlutterDetailBloc extends Bloc<FlutterDetailEvent, FlutterDetailState> {
   FlutterDetailBloc({
@@ -20,7 +20,7 @@ class FlutterDetailBloc extends Bloc<FlutterDetailEvent, FlutterDetailState> {
         super(const FlutterDetailState()) {
     on<FlutterDetailEvent>(
       (event, emit) async {
-        await event.when<Future<void>>(
+        await event.when(
           flutterWhatsNewDetailRequested: (id) {
             return _onFlutterWhatsNewDetailRequested(emit, id);
           },
@@ -34,7 +34,7 @@ class FlutterDetailBloc extends Bloc<FlutterDetailEvent, FlutterDetailState> {
 
   final FlutterDetailRepository _repository;
 
-  Future<void> _onFlutterWhatsNewDetailRequested(
+  Future<Unit> _onFlutterWhatsNewDetailRequested(
     Emitter<FlutterDetailState> emit,
     int id,
   ) {
@@ -45,7 +45,7 @@ class FlutterDetailBloc extends Bloc<FlutterDetailEvent, FlutterDetailState> {
     );
   }
 
-  Future<void> _onFlutterReleaseNotesDetailRequested(
+  Future<Unit> _onFlutterReleaseNotesDetailRequested(
     Emitter<FlutterDetailState> emit,
     int id,
   ) {
@@ -56,29 +56,38 @@ class FlutterDetailBloc extends Bloc<FlutterDetailEvent, FlutterDetailState> {
     );
   }
 
-  Future<void> _onFlutterDetailRequested(
+  Future<Unit> _onFlutterDetailRequested(
     _DetailFailureOrSuccess Function(int) caller,
     Emitter<FlutterDetailState> emit,
     int id,
   ) async {
     emit(state.copyWith(status: () => FlutterDetailStatus.loading));
-    final failureOrSuccessDetail = await caller(id);
-    return failureOrSuccessDetail.fold(
-      (failure) => emit(
-        state.copyWith(
-          status: () => FlutterDetailStatus.failure,
-          failureMessage: () => failure.when<String>(
-            api: (errorCode) => 'API returned $errorCode',
+    final failureOrSuccessDetail = await caller(id).run();
+    return failureOrSuccessDetail.match(
+      (failure) {
+        emit(
+          state.copyWith(
+            status: () => FlutterDetailStatus.failure,
+            failureMessage: () {
+              return switch (failure) {
+                ApiFailure() => failure.message,
+                UriParserFailure() => failure.message,
+              };
+            },
           ),
-        ),
-      ),
-      (detail) => emit(
-        state.copyWith(
-          status: () => FlutterDetailStatus.success,
-          detail: () => detail,
-          failureMessage: () => null,
-        ),
-      ),
+        );
+        return unit;
+      },
+      (detail) {
+        emit(
+          state.copyWith(
+            status: () => FlutterDetailStatus.success,
+            detail: () => detail,
+            failureMessage: () => null,
+          ),
+        );
+        return unit;
+      },
     );
   }
 }
