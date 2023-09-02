96f5ca27211e9c96b8d331724c7691555563a7f1
Biplab Dutta <biplabdutta860@gmail.com>
2023-09-03 00:41:17 +0545

    remove `detail_failure.dart` file

--- lib/features/detail/core/domain/detail_failure.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'detail_failure.freezed.dart';
-
-@freezed
-class DetailFailure with _$DetailFailure {
-  const factory DetailFailure.api(int? errorCode) = _Api;
-}
