04017d77bec00fe54ad436b266b0b45b4907b9c5
Biplab Dutta <biplabdutta860@gmail.com>
2023-09-02 23:39:41 +0545

    migrate `dart_detail_local_service.dart` to fpdart

--- lib/features/detail/dart_detail/data/dart_detail_local_service.dart
+++ lib/features/detail/dart_detail/data/dart_detail_local_service.dart
@@ -1,12 +1,13 @@
+import 'package:fpdart/fpdart.dart';
 import 'package:now_in_dart_flutter/features/detail/core/data/detail_dto.dart';
 import 'package:now_in_dart_flutter/features/detail/core/data/detail_local_service.dart';
 
 class DartDetailLocalService extends DetailLocalService {
   DartDetailLocalService({super.isarDb});
 
-  Future<void> upsertDartDetail(DetailDTO detailDTO) {
+  Task<Unit> upsertDartDetail(DetailDTO detailDTO) {
     return super.upsertDetail(detailDTO);
   }
 
-  Future<DetailDTO?> getDartDetail(int id) => super.getDetail(id);
+  Task<DetailDTO?> getDartDetail(int id) => super.getDetail(id);
 }
