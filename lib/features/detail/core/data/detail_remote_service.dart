887915260e6ceeb399620fe2978c6fe2a7e9e12f
Biplab Dutta <biplabdutta860@gmail.com>
2023-09-17 09:37:09 +0545

    refactor: move `_uriParser` function to `utils.dart`

--- lib/features/detail/core/data/detail_remote_service.dart
+++ lib/features/detail/core/data/detail_remote_service.dart
@@ -25,7 +25,7 @@ abstract class DetailRemoteService {
     return TaskEither.Do(
       (_) async {
         final requestUri = await _(
-          _uriParser(fullPathToMarkdownFile).toTaskEither(),
+          uriParser(fullPathToMarkdownFile).toTaskEither(),
         );
 
         final cachedHeader = await _(
@@ -83,15 +83,4 @@ abstract class DetailRemoteService {
       },
     );
   }
-}
-
-IOEither<Failure, Uri> _uriParser(String uri) {
-  return IOEither.tryCatch(
-    () => Uri.parse(uri),
-    (e, stackTrace) => UriParserFailure(
-      'Invalid Uri string',
-      errorObject: e,
-      stackTrace: stackTrace,
-    ),
-  );
 }
