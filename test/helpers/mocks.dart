1ff984bea205818228dd21794549c07887d4bfd0
Biplab Dutta <biplabdutta860@gmail.com>
2023-09-20 03:20:40 +0545

    test: write unit test for `detail_local_service.dart`

--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:isar/isar.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:now_in_dart_flutter/core/data/data.dart';
+import 'package:now_in_dart_flutter/features/detail/core/data/detail_local_service.dart';
 import 'package:now_in_dart_flutter/features/detail/core/data/detail_remote_service.dart';
 
 part 'fakes.dart';
@@ -16,3 +18,13 @@ class MockHeaderCache extends Mock implements HeaderCache {}
 class MockDetailRemoteService extends DetailRemoteService {
   MockDetailRemoteService({super.dio, super.headerCache});
 }
+
+class MockDetailLocalService extends DetailLocalService {
+  MockDetailLocalService({super.isarDb});
+}
+
+class MockIsarDatabase extends Mock implements IsarDatabase {}
+
+class MockIsar extends Mock implements Isar {}
+
+class MockIsarCollection<T> extends Mock implements IsarCollection<T> {}
